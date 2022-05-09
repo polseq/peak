553326e872f3bfb24fd099de8fbef8572a13fb0a
Emir Gocturk <emirgoppa@gmail.com>
2022-05-09 12:52:46 +0200

    Company and copyright information

--- peak/JuceLibraryCode/JuceHeader.h
+++ peak/JuceLibraryCode/JuceHeader.h
@@ -35,8 +35,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "peak";
-    const char* const  companyName    = "";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  companyName    = "polseq";
+    const char* const  versionString  = "0.0.1";
+    const int          versionNumber  = 0x1;
 }
 #endif
